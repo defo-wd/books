bb41c3d3a8e432eb74b23ac36a676bfc64b374db
EC2 Default User <ec2-user@ip-172-31-27-214.ap-southeast-2.compute.internal>
2023-07-07 10:21:08 +0000

    一覧画面（ホーム画面？）の作成、テーブルにして投稿された情報を表示できるように変更　IndexのなかにNewアクションのフォームを埋め込み

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,8 @@ class BooksController < ApplicationController
   end
 
   def index
-
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -23,7 +24,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-      end
+  end
 
 
   private

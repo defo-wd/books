ed6c0d646e00a818ea05f1244e3f45603acd1147
EC2 Default User <ec2-user@ip-172-31-27-214.ap-southeast-2.compute.internal>
2023-07-07 11:17:36 +0000

    あとスタートページを作って整えるだけ。。。？

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,21 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-   if @book.save
+    if @book.save
     redirect_to book_path(@book.id)
-
-  else
+    else
     render 'new'
     end
   end
+
+  def update
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    redirect_to book_path(@book.id)
+  else
+    render 'edit'
+  end
+end
 
   def index
     @books = Book.all
@@ -24,6 +32,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
 
